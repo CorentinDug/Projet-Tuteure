2b77083da4c6bf019cf43786c13662484d1fe088
Sevven <benji.esco@hotmail.fr>
2018-03-17 20:48:53 +0100

    Implémentation SCSS

---
+++ web/assets/js/script.js
@@ -0,0 +1,12 @@
+function cacheImageAfficheMap(image) {
+    document.getElementById("image").style.display = "none";
+    document.getElementById("map").style.display = "block";
+
+}
+
+
+function cacheMapAfficheImage() {
+    document.getElementById("map").style.display = "none";
+    document.getElementById("image").style.display = "block";
+
+}
